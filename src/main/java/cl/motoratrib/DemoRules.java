473c26f36c0bc5dc02bdc3f265fc52295138bee0
Marcelo Lomeña <marcelol@zrismart.cl>
2018-04-11 18:25:34 -0300

    nulo

---
+++ src/main/java/cl/motoratrib/DemoRules.java
@@ -0,0 +1,4 @@
+package cl.motoratrib;
+
+public class DemoRules {
+}
---
+++ src/main/java/cl/motoratrib/InJson.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class InJson {
+
+    private String rulesetName;
+    private List<Parameter> parameterList;
+	
+	public String getRulesetName() {
+		return rulesetName;
+	}
+	public void setRulesetName(String rulesetName) {
+		this.rulesetName = rulesetName;
+	}
+	
+	public List<Parameter> getParameterList() {
+		return parameterList;
+	}
+	public void setParameterList(List<Parameter> parameterList) {
+		this.parameterList = parameterList;
+	}
+	
+}
