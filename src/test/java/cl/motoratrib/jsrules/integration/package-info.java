4fe359c71aeff42fefd0fdbd171c0d9bf434cad1
Marcelo Lomeña <marcelol@zrismart.cl>
2018-09-24 17:34:17 -0300

    minimas chao

--- src/test/java/cl/motoratrib/jsrules/integration/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * This package contains integration tests, which perform end-to-end ruleset execution just as one might use in an
- * actual application. Not to be confused with the unit tests found in other packages.
- */
-package cl.motoratrib.jsrules.integration;
